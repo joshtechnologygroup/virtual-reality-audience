1b720b088cb600ff82b3af9f9a01541ee932b83c
Anshul Chhangani <anshul.chhangani@joshtechnologygroup.com>
2017-08-26 22:08:44 +0530

    Update sound feedback

--- app/src/main/java/com/example/ubuntu/myapplication/SoundFeedbackService.java
+++ app/src/main/java/com/example/ubuntu/myapplication/SoundFeedbackService.java
@@ -8,15 +8,42 @@ import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static com.google.android.gms.internal.zzagz.runOnUiThread;
+
 public class SoundFeedbackService extends Service {
     private MediaPlayer mediaPlayer;
+    private int maxClap = 5, maxBoo = 3;
+    private Timer timer;
+    private int time = 0;
+    private float totalPercentage;
 
     @Override
     public void onCreate() {
         super.onCreate();
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("mainServiceAction");
+        intentFilter.addAction("wpmServiceAction");
         registerReceiver(mReceiver, intentFilter);
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        time++;
+                    }
+                });
+            }
+        }, 1000, 1000);
+        startPlayer();
+
+    }
+
+    void startPlayer() {
         mediaPlayer = MediaPlayer.create(this, R.raw.background);
         mediaPlayer.start();
         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
@@ -30,7 +57,31 @@ public class SoundFeedbackService extends Service {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals("mainServiceAction")) {
-                float totalPercentage = intent.getFloatExtra("totalPercentage", 0);
+                totalPercentage = intent.getFloatExtra("totalPercentage", 0);
+            } else if (intent.getAction().equals("wpmServiceAction")){
+                if (totalPercentage > 70 && time > 10 && maxClap > 0) {
+                    time = 0;
+                    maxClap--;
+                    mediaPlayer.stop();
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.applause);
+                    mediaPlayer.start();
+                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            startPlayer();
+                        }
+                    });
+                } else if (totalPercentage > 30 && time > 10 && maxBoo > 0){
+                    time = 0;
+                    maxBoo--;
+                    mediaPlayer.stop();
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.chatter);
+                    mediaPlayer.start();
+                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            startPlayer();
+                        }
+                    });
+                }
             }
         }
     };
